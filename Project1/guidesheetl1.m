1862da7bba4e9dfe2237886cfbd40642193465d5
abisi <axel.bisi@epfl.ch>
2018-10-08 18:52:01 +0200

    Updated guidesheet 1 up to 3rrd question

--- Project1/guidesheetl1.m
+++ Project1/guidesheetl1.m
@@ -1,6 +1,37 @@
-histogram(trainData(:,500), 50, 'FaceColor', 'r', 'FaceAlpha', 0.5); hold on;
-histogram(trainData(:,1500), 50, 'FaceColor', 'y', 'FaceAlpha', 0.5);
+%guidesheet 1 : data exploration
+
+close all
+%statistical significance 
 
 C = categorical(trainLabels',[1 0],{'Error','Correct'});
 figure;
 histogram(C,'Normalization','probability', 'BarWidth',0.5);
+
+
+figure  %similar
+histogram(trainData(trainLabels == 0,650),50, 'Normalization', 'probability'); hold on
+histogram(trainData(trainLabels == 1,650),50, 'Normalization', 'probability'); hold off
+
+figure %different
+histogram(trainData(trainLabels == 0,712),50, 'Normalization', 'probability'); hold on
+histogram(trainData(trainLabels == 1,712),50, 'Normalization', 'probability'); hold off
+
+%% boxplot
+
+close all 
+
+figure
+boxplot([trainData(trainLabels == 0,650), trainData(trainLabels == 1,650)]); 
+
+figure
+boxplot([trainData(trainLabels == 0,720), trainData(trainLabels == 1,720)]); 
+
+
+%% boxplot with confidence interval
+
+figure
+boxplot([trainData(trainLabels == 0,650), trainData(trainLabels == 1,650)], 'Notch', 'on'); 
+
+figure
+boxplot([trainData(trainLabels == 0,720), trainData(trainLabels == 1,720)], 'Notch', 'on'); 
+
