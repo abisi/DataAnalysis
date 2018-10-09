2fe687d930f55a1c6e8a2d4d224fb8fc2608fe3e
julian-barry <julian_barry@yahoo.com>
2018-10-09 22:57:51 +0200

    update ; debut theshold

--- Project1/guidesheetl1.m
+++ Project1/guidesheetl1.m
@@ -1,5 +1,5 @@
-%guidesheet 1 : data exploration
-
+%% PART 1 : Statistical significance
+%% Histograms
 %closes previously open figures
 close all
 
@@ -12,7 +12,6 @@ C = categorical(trainLabels,[1 0],{'Error','Correct'});
 %apparaition to theoretical incidence rates : p(error)= 30% , p(correct)=
 %70%.
 histogram(trainLabels,'Normalization','probability', 'BinWidth',0.5);
-figure;
 
 %Examining data distribution for feature 650 in samples with classification
 %'correct'. Same thing for samples with classification 'incorrect'.
@@ -20,15 +19,15 @@ figure;
 %Similar distributions mean that the feature is not good for establishing
 %the classification. Highly disting distributions mean the feature is good
 %for establishing the classification.
+figure;
 histogram(trainData(trainLabels == 0,650),50, 'Normalization', 'probability'); hold on
 histogram(trainData(trainLabels == 1,650),50, 'Normalization', 'probability'); hold off
-figure;
 
 %Same thing, comparing data distribution @ feature 712 with respect to
 %classes.
+figure;
 histogram(trainData(trainLabels == 0,712),50, 'Normalization', 'probability'); hold on
 histogram(trainData(trainLabels == 1,712),50, 'Normalization', 'probability'); hold off
-figure;
 
 %% boxplot
 
@@ -45,13 +44,14 @@ ErrorTag = 2 * ones([nnz(trainLabels==1) 1]);
 groupingMatrix = [CorrectTag; ErrorTag];
 
 %boxplot of data distribution @feature 650 with respect to classes
+figure
 boxplot(trainData(:,650),groupingMatrix);
 %setting x-axis labels
 set(gca,'XTickLabel',{'Correct','Error'});
 
-%boxplot of data distribution @feature 720 with respect to classes
+%boxplot of data distribution @feature 712 with respect to classes
 figure
-boxplot(trainData(:,720), groupingMatrix); 
+boxplot(trainData(:,712), groupingMatrix); 
 set(gca,'XTickLabel',{'Correct','Error'});
 
 %% boxplots with confidence intervals
@@ -63,19 +63,19 @@ figure
 boxplot(trainData(:,650),groupingMatrix, 'Notch', 'on'); 
 
 %confidence intervals for respective means appear distinct. We suspect that
-%classes display significantly different data distributions @feature 720.
-%If significant, this difference means that feature 720 can be used to
+%classes display significantly different data distributions @feature 712.
+%If significant, this difference means that feature 712 can be used to
 %establish the desired classification.
 %Note that we still need to check statistical significance of mean
-%difference between classes @ feature 720 using t-testing.
+%difference between classes @ feature 712 using t-testing.
 figure
-boxplot(trainData(:,720), groupingMatrix, 'Notch', 'on'); 
+boxplot(trainData(:,712), groupingMatrix, 'Notch', 'on'); 
 
 %% t-tests
 
 %Sets of Data to be compared
-CorrectClassDataF720=trainData(trainLabels==0,720);
-ErrorClassDataF720=trainData(trainLabels==1,720);
+CorrectClassDataF712=trainData(trainLabels==0,712);
+ErrorClassDataF712=trainData(trainLabels==1,712);
 
 CorrectClassDataF650=trainData(trainLabels==0,650);
 ErrorClassDataF650=trainData(trainLabels==1,650);
@@ -86,15 +86,27 @@ ErrorClassDataF650=trainData(trainLabels==1,650);
 %boxplots.
 
 %Null hypothesis (average of both data sets is equal) can be
-%rejected @f720 at 5% significance lvl. It cannot be rejected for f650.
+%rejected @f712 at 5% significance lvl. It cannot be rejected for f650.
 
 %p-value (probability of observing the given result, or one more extreme,
-%by chance if the null hypothesis is true) for f720 is extermely low
-%(0.0009). For f650, the p-value is very large (0.4).
+%by chance if the null hypothesis is true) for f712 is extermely low
+%(3.8e-16). For f650, the p-value is very large (0.4).
 
 %One cannot simply use t-test for any feature. First we need to check that
 %data from respective classes is normally distributed at this feature and
 %that their variance is similar (histogram superposition).
 
 [H_f650,P_f650]=ttest2(CorrectClassDataF650,ErrorClassDataF650);
-[H_f720,P_f720]=ttest2(CorrectClassDataF720,ErrorClassDataF720);
+[H_f712,P_f712]=ttest2(CorrectClassDataF712,ErrorClassDataF712);
+%% PART 2 : Feature Thresholding
+%% Determining optimal theshold through histograms
+close all
+%Class histograms for feature 712 with threshold
+figure;
+histogram(trainData(trainLabels == 0,712),100, 'Normalization', 'probability'); hold on
+histogram(trainData(trainLabels == 1,712),100, 'Normalization', 'probability');
+threshold=line([0.64 0.64], [0 0.04]);
+set(threshold,'LineWidth',4,'color','red');
+
+
+
