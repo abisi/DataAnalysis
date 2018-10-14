ee7a0ace2248276671668842303ff6e888f5992b
julian-barry <julian_barry@yahoo.com>
2018-10-14 22:58:14 +0200

    update
    jusqu'au scaling a multiples dimensions

--- Project1/guidesheetl1.m
+++ Project1/guidesheetl1.m
@@ -100,7 +100,11 @@ ErrorClassDataF650=trainData(trainLabels==1,650);
 [H_f712,P_f712]=ttest2(CorrectClassDataF712,ErrorClassDataF712);
 %% PART 2 : Feature Thresholding
 %% Determining optimal theshold through histograms
-close all
+close all;
+clear all;
+load('trainSet.mat');
+load('trainLabels.mat');
+
 %Class histograms for feature 712 with threshold
 figure;
 histogram(trainData(trainLabels == 0,712),100, 'Normalization', 'probability'); hold on
@@ -108,5 +112,87 @@ histogram(trainData(trainLabels == 1,712),100, 'Normalization', 'probability');
 threshold=line([0.64 0.64], [0 0.04]);
 set(threshold,'LineWidth',4,'color','red');
 
+%Class histograms for feature 720 with threshold
+figure
+histogram(trainData(trainLabels == 0,720),100, 'Normalization', 'probability'); hold on
+histogram(trainData(trainLabels == 1,720),100, 'Normalization', 'probability');
+threshold=line([0.47 0.47], [0 0.05]);
+set(threshold,'LineWidth',4,'color','red');
+
+%Class histograms help us to optimally place threshold because we can
+%minimize false classifications by placing threshold at intersection of
+%conditional probability distributions.
+%% Plot of datapoints of two features with optimal threashold as a line
+figure
+correct712=plot(trainData(trainLabels==0,712));hold on
+error712=plot(trainData(trainLabels==1,712));
+correct720=plot(trainData(trainLabels==0,720));
+error720=plot(trainData(trainLabels==1,720));
+set(correct712,'color','blue');
+set(error712,'color','green');
+set(correct720,'color','yellow');
+%set(error720,'color','purple');
+%-> Cannot set color here because pixels color is already set previously?
+threshold=line([0 500], [0.64 0.64]);
+set(threshold,'LineWidth',4,'color','red');
+
+%We see that for feature 712 (feature used to define threshold), correct
+%class (blue)is predominantly below our threashold whereas error class
+%(green) is predominantly above.
+
+%Unfortunately, we see that data from other features such as feature 720
+%does not obey our established threashold. We see that both error class
+%(purple) and correct class (yellow) lie predominantly below the threashold.
+
+% We conclude that the shortcoming of feature thresholding is that we
+% cannot establish a universal threashold that holds for any feature within
+% our data.
+%% classification error
+close all;
+clear all;
+load('trainSet.mat');
+load('trainLabels.mat');
+
+threshVal=0.64;
+%predicting
+predicted=[];
+for i=1:597
+    if(trainData(i,712)>0.64)
+        predicted(i)=1;
+    else
+        predicted(i)=0;
+    end
+end
+%comparing predicted to labels
+correctCounter=0;
+for i=1:597
+    if(predicted(1,i)==trainLabels(i,1))
+        correctCounter=correctCounter+1;
+    end
+end
+%classification accuracy
+classificationAccuracy=correctCounter/597;
+%classification error
+classificationError=1-classificationAccuracy;
+%error typing
+corrError=0;
+errError=0;
+for i=1:597
+    if(predicted(1,i)~=trainLabels(i,1) && trainLabels(i,1)==0)
+        corrError=corrError+1;
+    else if (predicted(1,i)~=trainLabels(i,1) && trainLabels(i,1)==1)
+            errError=errError+1;
+        end
+    end
+end
+%classError
+numberErr=nnz(trainLabels);
+numberCorr=597-numberErr;
+classError=0.5*(errError/numberErr)+0.5*(corrError/numberCorr);
+
+%cleanup
+clear correctCounter corrError errError i numberCorr numberErr predicted;
+
+%% Sclaing thresholding method to multiple dimensions
 
 
