84f2ae14969a368266faa01a1172a7621ec2ae47
giustogianni <gianni.giusto@epfl.ch>
2018-10-18 17:01:17 +0200

    Update part 1 of guidesheet2
    Clean code and add bar plot to compare classifier (linear, diaglin, quadratic, diagquadratic).
    Comparison between the 2 types of errors.

--- Project1/guidesheet2.m
+++ Project1/guidesheet2.m
@@ -32,6 +32,81 @@ yhatDiagQuad=predict(classifierDiagQuad,features);
 %[classErrorQuad] = computeClassError(trainLabels, yhatQuad, 0.5);
 [classErrorDiagQuad] = computeClassError(trainLabels, yhatDiagQuad, 0.5);
 
+
+%% Version 2.0
+%If this part of the code is correct, we can remove all the part above.
+%Small variations by comparing Class errors and classification errors >
+%check together
+
+clear all;
+load('trainSet.mat');
+load('trainLabels.mat');
+labels=trainLabels;
+features=trainData(:,1:10:end);
+
+ratio=0.5;
+
+classifierTypes=["linear","diaglinear","diagquadratic"]; %"quadratic" not used because singular problem 
+
+%Compute classification error and class error for classifier: 
+classificationErrors=[];
+classErrors=[];
+
+priorProba="uniform";
+classificationErrors_priorProba=[];
+classErrors_priorProba=[];
+
+for i=1:length(classifierTypes)
+   ypred=classifierPrediction(features, labels, classifierTypes(i));
+  
+   classificationError=computeClassificationError(labels, ypred);
+   classificationErrors=[classificationErrors classificationError];
+   
+   classError = computeClassError(labels, ypred, ratio);
+   classErrors = [classErrors, classError];
+   
+   %Case with prior probability = uniform
+   ypred_priorProba=classifierPrediction(features, labels, classifierTypes(i), priorProba); %specifiy the prior proba to the function
+   
+   classificationError_priorProba=computeClassificationError(labels, ypred_priorProba);
+   classificationErrors_priorProba=[classificationErrors_priorProba classificationError_priorProba];
+   
+   classError_priorProba = computeClassError(labels, ypred_priorProba, ratio);
+   classErrors_priorProba = [classErrors_priorProba, classError_priorProba];
+   
+end
+
+%Display results - Classification error
+figure;
+bar(classificationErrors);
+set(gca,'XTickLabel',{'Linear','Diag Linear', 'Diag Quadratic'});
+ylabel('Classification Error');
+title('Classification error for different classifier methods');
+
+%Display results - Class error
+figure;
+bar(classErrors);
+set(gca,'XTickLabel',{'Linear','Diag Linear', 'Diag Quadratic'});
+ylabel('Class Error');
+title(['Class error for different classifier methods (ratio= ', num2str(ratio), ')' ]);
+
+
+%Display results - Classification error, prior probability = uniform
+figure;
+bar(classificationErrors_priorProba);
+set(gca,'XTickLabel',{'Linear','Diag Linear', 'Diag Quadratic'});
+ylabel('Class Error');
+title({'Classification error for different classifier methods'; 'Prior probability: uniform'});
+
+%Display results - Class error, prior probability = uniform
+figure;
+bar(classErrors_priorProba);
+set(gca,'XTickLabel',{'Linear','Diag Linear', 'Diag Quadratic'});
+ylabel('Class Error');
+title({['Class error for different classifier methods (ratio= ', num2str(ratio), ')' ]; 'Prior probability: uniform'});
+
+
+
 %% Training and testing error
 %clearing previous vars
 clear all;
