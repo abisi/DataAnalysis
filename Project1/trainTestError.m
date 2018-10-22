9b8c51fe1a0bd000d28de62b459c2a32015009dc
abisi <axel.bisi@epfl.ch>
2018-10-22 13:06:06 +0200

    Formatted trainTestError

--- Project1/trainTestError.m
+++ Project1/trainTestError.m
@@ -1,8 +1,18 @@
-function [ error] = trainTestError( trainSet, trainLabels, testSet, testLabels, model, ratio )
-% Given a model trained on training dataset, computes train and test class
-% errors with specified ratio. Real data classification sets, trainLabels
-% and testLabels are necessary to compute errors.
-% Output is of format error=[trainError,testError]. 
+function [error] = trainTestError(trainSet, trainLabels, testSet, testLabels, model, ratio )
+% TRAINTESTERROR Both predicts and computes class error using given
+%                training set et test set.
+% 	Given a model trained on training dataset, computes train and test class
+%   errors with specified ratio. Real data classification sets, trainLabels
+%   and testLabels are necessary to compute errors.
+%   INTUTS:
+%       trainSet: training set with features
+%       trainLabels: training labels
+%       testSet: test set with features
+%       testLabels: test labels (often not available)
+%       model: classifier model (fitcdiscr)
+%       ratio: ratio used for class error
+%   OUTPUT:  ERROR=[TRAINERROR, TESTERROR]. 
+
 trainPrediction = predict(model, trainSet);
 testPrediction = predict(model,testSet);
 
