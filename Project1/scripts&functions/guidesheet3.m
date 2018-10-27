e1e4ae06b1a984d7f83961a9734449fea905c880
julian-barry <julian_barry@yahoo.com>
2018-10-27 19:58:33 +0200

    guidesheet 3

--- Project1/scripts&functions/guidesheet3.m
+++ Project1/scripts&functions/guidesheet3.m
@@ -3,19 +3,19 @@
 %%Cross-validation for hyperparameter optimization
 
 %Fisher-based feature selection
-k = 10;
-maxFt = 20;
-cp_labels = cvpartition(trainLabels, 'kfold', k);
-trainErrors = zeros(k, maxFt);
+k = 10; %number of folds
+maxFt = 20; %max number of features
+cp_labels = cvpartition(trainLabels, 'kfold', k);%partition
+trainErrors = zeros(k, maxFt); %initialization
 testErrors = zeros(k, maxFt);
 
-for ft = 1:maxFt
+for ft = 1:maxFt %incrementing number of features
     for i=1:cp_labels.NumTestSets
-        trainId = cp_labels.training(i);
-        testId = cp_labels.test(i);
+        trainId = cp_labels.training(i); %markers for training
+        testId = cp_labels.test(i); %markers for testing
         %Pick ft-th best features
     	[orderedInd, orderedPower] = rankfeat(trainData(trainId,:), trainLabels(trainId,:), 'fisher');
-        bestInd = orderedInd(1:ft);
+        bestInd = orderedInd(1:ft); %selection of best n features
         %Build model and predict
         classifierDiagLin = fitcdiscr(trainData(trainId, bestInd), trainLabels(trainId,:), 'DiscrimType', 'diaglinear');
         yhatTrain = predict(classifierDiagLin, trainData(trainId, bestInd));
@@ -24,16 +24,16 @@ for ft = 1:maxFt
         errTrain = computeClassificationError(trainLabels(trainId,:), yhatTrain);
         errTest = computeClassificationError(trainLabels(testId,:), yhatTest);
         %Store 
-        trainErrors(i, ft) = errTrain;
-        testErrors(i, ft) = errTest;
+        trainErrors(i, ft) = errTrain; % Each column respresents a different model (different amount of features used)
+        testErrors(i, ft) = errTest; % Each row of selected column contains different fold respective errors.
     end
 end
 
 %Average errors (+std)
-meanTrainErrors = mean(trainErrors)
+meanTrainErrors = mean(trainErrors) %mean of a matrix returns row vector containing mean value of each column
 meanTestErrors = mean(testErrors)
 
-stdTrainErrors = std(trainErrors);
+stdTrainErrors = std(trainErrors); % returns std dev of each column in a row vector
 stdTestErrors = std(testErrors);
 
 %Let's plot this
