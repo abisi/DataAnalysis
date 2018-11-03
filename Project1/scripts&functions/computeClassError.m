e821f4e7f4b21eb87cd3a182e136e684371aa032
abisi <axel.bisi@epfl.ch>
2018-11-03 17:12:42 +0100

    Modified class error function, now does NOT take ratio but calculate within

--- Project1/scripts&functions/computeClassError.m
+++ Project1/scripts&functions/computeClassError.m
@@ -1,20 +1,22 @@
-function [classError] = computeClassError(labels, prediction, ratio)
+function [error, accur] = computeClassError(labels, prediction)
 %CLASSERROR Computes class error.
 %   For unbalanced classes A and B across a dataset, the class error is defined in
 %   order to balance the influence of errors in class A and class B.
 %   labels : true target labels
 %   prediction : predicted classes
-%   ratio : number between 0 and 1 for the proportion of CLASS A (class
-%   Correct), then for second factor : [1 - ratio]
+%
 
+
+% Ratio of correct '0'
+ratio = length((find(labels == 0))) / length(labels);
 % Error counters:
 correctClassError = 0;
 errorClassError = 0;
 
 for i=1:length(labels)
-    if(prediction(i)~=labels(i) && labels(i) == 0)
+    if(prediction(i) ~= labels(i) && labels(i) == 0)
         correctClassError = correctClassError + 1;
-    else if (prediction(i)~=labels(i) && labels(i)== 1)
+    else if (prediction(i) ~= labels(i) && labels(i) == 1)
             errorClassError = errorClassError + 1;
         end
     end
@@ -23,7 +25,9 @@ end
 nErr=nnz(labels); %because 1 corresponds to error class
 nCorr=length(labels)-nErr;
 
-classError = ratio .* (correctClassError/nCorr) + (1-ratio) .* (errorClassError/nErr);
+error = ratio * (correctClassError/nCorr) + (1-ratio) * (errorClassError/nErr);
+accur = 1 - error;
+
 end
 
 
