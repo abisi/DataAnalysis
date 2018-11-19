449dfa07aa8ac37e5e4945bc883ea62e1afa807e
abisi <axel.bisi@epfl.ch>
2018-11-19 17:42:49 +0100

    Again changes

--- Project2/scripts/guidesheet7.m
+++ Project2/scripts/guidesheet7.m
@@ -2,23 +2,17 @@
 
 clear all;
 close all;
-load('../data/data.mat');
+load('../data/Data.mat');
 
 %% Data set partitioning and PCA 
 
 %Partition
 proportion = 0.7;     
-rows = size(Data,1);  
-idx_vector = false(rows,1);    
-idx_vector(1:round(proportion*rows)) = true;   
-idx_vector = idx_vector(randperm(rows));   % randomise order
-train = Data(idx_vector,:);
-test = Data(~idx_vector,:);
+rows = size(Data,1);    
+train = Data(1:round(rows*proportion),:);
+test = Data(rows-round(rows*(1-proportion)):end,:);
 
 %PCA
-
-%[std_train, mu, sigma] = zscore(train); NO because time-correlated
-%std_test = (test - mu ) ./ sigma; 
 
 [coeff, score, latent] = pca(train);
 pca_train = train * coeff;
@@ -42,14 +36,15 @@ figure
 bar(latent);
 
 %% Regression - linear
-chosen_PCs = 578; %TO FILL IN
+chosen_PCs = 579; %TO FILL IN
 target_posx = PosX(train); %y
 target_posy = PosY(train);
 
-FM = pca_train(:,1:chosen_PCs); 
+FMx = pca_train(target_posx,1:chosen_PCs); 
+FMy = pca_train(target_posy,1:chosen_PCs); 
 
-Ix = ones(size(target_posx,1),1);
-Iy = ones(size(target_posy,1),1);
+I = ones(size(target_posx,1),1);
+
 X_posx = [Ix FM];
 X_posy = [Iy FM];
 
@@ -61,7 +56,7 @@ mse_posx = immse(target_posx, X_posx * bx);
 mse_posy = immse(target_posy, X_posy * by);
 
 %Plot real vectors and regressed ones
-
+plot(ms)
 
 %% Regression - 2nd order polynomial regressor
 X_posx_order2 = [Ix feature_matrix feature_matrix.^2];
