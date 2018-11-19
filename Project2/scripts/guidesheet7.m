387ace9d700e7e222b6744ee458cbd099e1120ed
giustogianni <gianni.giusto@epfl.ch>
2018-11-19 15:39:23 +0100

    Update guidesheet 7

--- Project2/scripts/guidesheet7.m
+++ Project2/scripts/guidesheet7.m
@@ -24,14 +24,37 @@ std_test = (test - mu ) ./ sigma;
 [coeff, score, latent] = pca(std_train);
 pca_data = std_train * coeff;
 
+pca_test = std_test * coeff;
+
 %Choose PCs (+ graph)
 
 
-%Regression
-
-%chosen_PCs = ...; TO FILL IN
-target_posx = PosX(train);
+%Regression - linear
+%chosen_PCs = ...; %TO FILL IN
+target_posx = PosX(train); %y
 target_posy = PosY(train);
 
-b = regress(target_posx(train),data(train,chosen_PCs))
+%feature_matrix = ...; %TO FILL IN
+I = ones(size(target_posx(train),1),1);
+%For the x-data vector
+X_posx = [I feature_matrix];
+
+bx = regress(target_posx(train),X_posx(train,chosen_PCs)); %b: coefficient
+
+%For the y-data vector
+X_posy = [I feature_matrix];
+
+by = regress(target_posy(train),X_posy(train,chosen_PCs));
+
+%Mean-square error calculation
+mse_posx = immse(target_posx, X_posx * bx);
+mse_posy = immse(target_posy, X_posy * by);
+
+
+%Plot real vectors and regressed ones
+
+
+%Regression - 2nd order polynomial regressor
+X_posx_order2 = [Ix feature_matrix feature_matrix.^2];
+X_posy_order2 = [Iy feature_matrix feature_matrix.^2];
 
