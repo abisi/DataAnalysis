94f11512e131e9214288457f3e2fd26a8598971a
julian-barry <julian_barry@yahoo.com>
2018-11-26 19:25:09 +0100

    commit pour gianni

--- Project2/scripts/guidesheet8.m
+++ Project2/scripts/guidesheet8.m
@@ -4,28 +4,34 @@ clear all;
 close all;
 load('../data/Data.mat');
 
-%% Regression
-
-%Data partitionning
-proportion = 0.05; %use 5% of the dataset for training     
+%% Data partitionning
+%For the report: change the values!
+train_proportion = 0.05; %use 5% of the dataset for training
+validation_proportion = 0.65;
 rows = size(Data,1);
-sep_idx = round(rows*proportion);
-train = Data(1:sep_idx,:); % We only use 5% of Data for training!!
-test = Data(sep_idx:end,:);
+sep_idx_train = ceil(rows*train_proportion);
+sep_idx_val = ceil(rows*(train_proportion + validation_proportion));
+
+%% Train vs Test sets
+train = Data(1:sep_idx_train,:);
+validation = Data(sep_idx_train+1:sep_idx_val,:);
+test = Data(sep_idx_val+1:end,:);
 
 %Train outputs
-pos_x = PosX(1:sep_idx);
-pos_y = PosY(1:sep_idx);
+pos_x = PosX(1:sep_idx_train);
+pos_y = PosY(1:sep_idx_train);
 
 %Test outputs
-pos_x_test = PosX(sep_idx:end);
-pos_y_test = PosY(sep_idx:end);
+pos_x_test = PosX(sep_idx_train+1:end);
+pos_y_test = PosY(sep_idx_train+1:end);
 
-%Regression
-nFeatures = 100; %TODO: define appropriate value -> NCV?
+%% Regression
+%For the purpose of the exercice we'll split thee data in 3 sets:
+%5% training, 65% validation, 30% test
+nFeatures = size(train,2); %TODO: define appropriate feature number
 %Train: for PosX and PosY
 FM_train = train(:, 1:nFeatures);
-I_train = ones(size(FM_train, 1), 1);
+I_train = ones(size(pos_x, 1), 1);
 X_train = [I_train FM_train];
 
 bx = regress(pos_x, X_train);
@@ -34,13 +40,16 @@ by = regress(pos_y, X_train);
 x_hat = X_train * bx; %regression vectors
 y_hat = X_train * by;
 
-mse_posx = immse(pos_x, x_hat); 
-mse_posy = immse(pos_y, y_hat); 
+mse_posx_train = immse(pos_x, x_hat); 
+mse_posy_train = immse(pos_y, y_hat); 
 
 %Test:for PosX and PosY
-FM_test = test(:, 1:nFeatures);
-I_test = ones(size(FM_test, 1), 1);
-X_test = [I_test FM_test];
+%FM = validation + test
+FM = Data(sep_idx_train+1:end,:);
+I = ones(floor((1-train_proportion) * rows), 1);
+disp(size(FM))
+disp(size(I))
+X_test = [I FM];
 
 x_hat_test = X_test * bx;
 y_hat_test = X_test * by;
@@ -48,14 +57,37 @@ y_hat_test = X_test * by;
 mse_posx_test = immse(pos_x_test, x_hat_test); 
 mse_posy_test = immse(pos_y_test, y_hat_test); 
 
-%plot...
+%plot
+figure()
+scatter(pos_x, pos_y, 'k', 'LineWidth', 4); hold on;
+scatter(x_hat, y_hat, 'g'); hold off;
+
+figure()
+scatter(pos_x_test(1:50:end), pos_y_test(1:50:end), 'k', 'LineWidth', 4); hold on;
+scatter(x_hat_test(1:50:end), y_hat_test(1:50:end), 'r');
+
+%probable overfit
 
 
 %% LASSO
+
+%Data partitionning
+%Train outputs
+pos_x = PosX(1:sep_idx_train);
+pos_y = PosY(1:sep_idx_train);
+
+%Validation outputs
+pox_x_val = PosX(sep_idx_train+1:sep_idx_val);
+pos_y_val = PosY(sep_idx_train+1:sep_idx_val);
+
+%Test outputs
+pos_x_test = PosX(sep_idx_val+1:end);
+pos_y_test = PosY(sep_idx_val+1:end);
+
 lambda = logspace(-10, 0, 15);
 k = 10;
-[Bx, FitInfo_x] = lasso(train, pos_x, 'Lambda', lambda, 'CV', k);
-[By, FitInfo_y] = lasso(train, pos_y, 'Lambda', lambda, 'CV', k);
+[Bx, FitInfo_x] = lasso(train, pos_x_train, 'Lambda', lambda, 'CV', k);
+[By, FitInfo_y] = lasso(train, pos_y_train, 'Lambda', lambda, 'CV', k);
 
 %Number of non-zero beta weight
 nz_weight=[]; %contain non-zero element for each lambda
@@ -84,6 +116,5 @@ beta_x = Bx(:, min_mse_idx);
 
 %% Elastic nets
 
-[Bx, FitInfo_x] = lasso(train, pos_x, 'Lambda', lambda, 'CV', k, 'Alpha', 0.5);
-[By, FitInfo_y] = lasso(train, pos_y, 'Lambda', lambda, 'CV', k, 'Alpha', 0.5);
-
+[Bx_en, FitInfo_x_en] = lasso(train, pos_x, 'Lambda', lambda, 'CV', k, 'Alpha', 0.5);
+[By_en, FitInfo_y_en] = lasso(train, pos_y, 'Lambda', lambda, 'CV', k, 'Alpha', 0.5);
